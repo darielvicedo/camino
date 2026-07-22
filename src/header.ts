965b261e436aad98e45dbea6530e05bf4ff17dfb
Dariel Vicedo <darielvicedo@gmail.com>
2026-07-22 16:44:23 -0400

    improvement: Format html string

--- src/header.ts
+++ src/header.ts
@@ -3,12 +3,12 @@ import viteLogo from "./assets/vite.svg";
 export default function renderHeader(): string {
   return /*html*/ `
     <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">
-      <img src="${viteLogo}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      El Camino de San Carlos
-    </a>
-  </div>
-</nav>
-    `;
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="${viteLogo}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                El Camino de San Carlos
+            </a>
+        </div>
+    </nav>
+`;
 }
